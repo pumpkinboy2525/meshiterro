6a73d6c94ed6ff4d95e491e28e7accc7adcaa0ab
EC2 Default User <ec2-user@ip-172-31-32-194.ap-northeast-1.compute.internal>
2024-01-20 05:50:36 +0000

    2-10

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+
+  
+  resources :post_images, only: [:new, :create, :index, :show]
+  devise_for :users
+  root to: "homes#top"
+  get '/about' => "homes#about"
+  
+end
